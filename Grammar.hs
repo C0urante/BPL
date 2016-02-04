8138791e619a7dceb76567230d52b3f0326e8caf
C0urante <cegerton@oberlin.edu>
2016-02-03 20:13:41 -0500

    Fix bug involving ArgList definition, add derived instances for Eq and Show

--- Grammar.hs
+++ Grammar.hs
@@ -1,3 +1,10 @@
+-- Chris Egerton
+-- February 3, 2016
+
+--      This is the Grammar module.
+--      Its sole purpose is to provide definitions for datatypes used in
+-- representing the grammar of BPL programmatically.
+
 module Grammar where
 
 import Token
@@ -7,45 +14,70 @@ data VarDecMetaType =
     RawVarDec |
     PointerVarDec |
     ArrayVarDec Int
-
+    deriving (Eq, Show)
 data ParamMetaType =
     RawParam |
     PointerParam |
     ArrayParam
-
+    deriving (Eq, Show)
 data VarMetaType =
     RawVar |
     PointerVar |
     ArrayVar Expression
+    deriving (Eq, Show)
 
 -- 1. PROGRAM -> DECLARATION_LIST
-data Program = Program DeclarationList
+data Program =
+    Program DeclarationList
+    deriving (Eq, Show)
 -- 2. DECLARATION_LIST -> DECLARATION_LIST DECLARATION | DECLARATION
-data DeclarationList = DeclarationList [Declaration]
+data DeclarationList =
+    DeclarationList [Declaration]
+    deriving (Eq, Show)
 -- 3. DECLARATION -> VAR_DEC | FUN_DEC
-data Declaration = VarDecDeclaration VarDec | FunDecDeclaration FunDec
+data Declaration =
+    VarDecDeclaration VarDec | FunDecDeclaration FunDec
+    deriving (Eq, Show)
 -- 4. VAR_DEC -> TYPE_SPECIFIER <id> ;
 --             | TYPE_SPECIFIER *<id> ;
 --             | TYPE_SPECIFIER <id>[ <num> ] ;
-data VarDec = VarDec TypeSpecifier VarDecMetaType Identifier
+data VarDec =
+    VarDec TypeSpecifier VarDecMetaType Identifier
+    deriving (Eq, Show)
 -- 5. TYPE_SPECIFIER -> int | void | string
-data TypeSpecifier = Number | Void | String
+data TypeSpecifier =
+    Number | Void | String
+    deriving (Eq, Show)
 -- 6. FUN_DEC -> TYPE_SPECIFIER <id> ( PARAMS ) COMPOUND_STMT
-data FunDec = FunDec TypeSpecifier Identifier Params CompoundStmt
+data FunDec =
+    FunDec TypeSpecifier Identifier Params CompoundStmt
+    deriving (Eq, Show)
 -- 7. PARAMS -> void | PARAM_LIST
-data Params = Params ParamList | EmptyParams
+data Params =
+    Params ParamList | EmptyParams
+    deriving (Eq, Show)
 -- 8. PARAM_LIST -> PARAM_LIST , PARAM | PARAM
-data ParamList = ParamList [Param]
+data ParamList =
+    ParamList [Param]
+    deriving (Eq, Show)
 -- 9. PARAM -> TYPE_SPECIFIER <id>
 --           | TYPE_SPECIFIER *<id>
 --           | TYPE_SPECIFIER <id>[ ]
-data Param = Param TypeSpecifier ParamMetaType Identifier
+data Param =
+    Param TypeSpecifier ParamMetaType Identifier
+    deriving (Eq, Show)
 -- 10. COMPOUND_STMT -> { LOCAL_DECS STATEMENT_LIST }
-data CompoundStmt = CompoundStmt LocalDecs StatementList
+data CompoundStmt =
+    CompoundStmt LocalDecs StatementList
+    deriving (Eq, Show)
 -- 11. LOCAL_DECS -> LOCAL_DECS VAR_DEC | <empty>
-data LocalDecs = LocalDecs [VarDec] | EmptyLocalDecs
+data LocalDecs =
+    LocalDecs [VarDec] | EmptyLocalDecs
+    deriving (Eq, Show)
 -- 12. STATEMENT_LIST -> STATEMENT_LIST STATEMENT | <empty>
-data StatementList = StatementList [Statment] | EmptyStatementList
+data StatementList =
+    StatementList [Statement] | EmptyStatementList
+    deriving (Eq, Show)
 
 -- 13. STATEMENT -> EXPRESSION_STMT
 --                | COMPOUND_STMT
@@ -65,13 +97,20 @@ data Statement =
     WhileStmt Expression Statement |
     ReturnStmt Expression | EmptyReturnStmt |
     WriteStmt Expression | WritelnStmt
+    deriving (Eq, Show)
 
 -- -- 19. EXPRESSION -> VAR = EXPRESSION | COMP_EXP
-data Expression = AssignmentExpression Var Expression | Expression CompExp
+data Expression =
+    AssignmentExpression Var Expression | Expression CompExp
+    deriving (Eq, Show)
 -- 20. VAR -> <id> | <id>[ EXPRESSION ] | *<id>
-data Var = Var Identifier VarMetaType
+data Var =
+    Var Identifier VarMetaType
+    deriving (Eq, Show)
 -- 21. COMP_EXP -> E RELOP E | E
-data CompExp = CompExp E Relop E | SimpleExp E
+data CompExp =
+    CompExp E Relop E | SimpleExp E
+    deriving (Eq, Show)
 -- 22. RELOP -> <= | < | == | != | > | >=
 data Relop =
     LessThanOrEqualRelop |
@@ -80,16 +119,27 @@ data Relop =
     NotEqualRelop |
     GreaterThanRelop |
     GreaterThanOrEqualRelop
+    deriving (Eq, Show)
 -- 23. E -> E ADDOP T | T
-data E = AddE E AddOp T | SimpleE T
+data E =
+    AddE E AddOp T | SimpleE T
+    deriving (Eq, Show)
 -- 24. ADDOP -> + | -
-data AddOp = PlusAddOp | MinusAddOp
+data AddOp =
+    PlusAddOp | MinusAddOp
+    deriving (Eq, Show)
 -- 25. T -> T MULOP F | F
-data T = MulT T MulOp F | SimpleT F
+data T =
+    MulT T MulOp F | SimpleT F
+    deriving (Eq, Show)
 -- 26. MULOP -> * | / | %
-data MulOp = TimesMulOp | DivMulOp | ModMulOp
+data MulOp =
+    TimesMulOp | DivMulOp | ModMulOp
+    deriving (Eq, Show)
 -- 27. F -> -F | &Factor | *Factor | Factor
-data F = NegativeF F | ReferenceF Factor | DereferenceF Factor | SimpleF Factor
+data F =
+    NegativeF F | ReferenceF Factor | DereferenceF Factor | SimpleF Factor
+    deriving (Eq, Show)
 -- 28. Factor -> ( EXPRESSION ) |
                 --  FUN_CALL |
                 --  read ( ) |
@@ -98,22 +148,28 @@ data F = NegativeF F | ReferenceF Factor | DereferenceF Factor | SimpleF Factor
                 --  <id>[EXPRESSION] |
                 --  <num> |
                 --  <string>
-data Factor = ExpressionFactor Expression |
-              FunCallFactor FunCall |
-              ReadFactor |
-              DereferenceFactor Identifier |
-              VarFactor Identifier |
-              ArrayReferenceFactor Identifier Expression |
-              NumberFactor Int |
-              StringFactor String
+data Factor =
+    ExpressionFactor Expression |
+    FunCallFactor FunCall |
+    ReadFactor |
+    DereferenceFactor Identifier |
+    VarFactor Identifier |
+    ArrayReferenceFactor Identifier Expression |
+    NumberFactor Int |
+    StringFactor String
+    deriving (Eq, Show)
 -- 29. FUN_CALL -> <id> ( ARGS )
-data FunCall = FunCall Identifier Args
+data FunCall =
+    FunCall Identifier Args
+    deriving (Eq, Show)
 -- 30. ARGS -> ARG_LIST | <empty>
-data Args = Args ArgList | EmptyArgs
+data Args =
+    Args ArgList | EmptyArgs
+    deriving (Eq, Show)
 -- 31. ARG_LIST -> ARG_LIST , EXPRESSION | EXPRESSION
-data ArgList = [Expression]
-
-
+data ArgList =
+    ArgList [Expression]
+    deriving (Eq, Show)
 
 
 -- data Statement =
